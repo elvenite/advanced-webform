5884b5f22cbc7293eeed567ea8af96145bfb64cc
Carl-Fredrik Herö <carlfredrik.hero@gmail.com>
2014-10-02 21:59:12 +0200

    changed Text field to accept other types (like password and email)

--- src/Elements/Text.php
+++ src/Elements/Text.php
@@ -42,88 +42,88 @@ namespace AdvancedWebform\Elements;
  */
 class Text extends Element{
 	
-	public function __construct($app_field, $form, $item_field = null) {
-		parent::__construct($app_field, $form, $item_field);
-		
-		// set type
-		if ($this->app_field->config['settings']['size'] == 'small'){
-			$type = 'text';
-			
-		} else {
-			$type = 'textarea';
-			$this->set_attribute('rows', 6);
-			if ($item_field){
-				$value = $item_field->values[0]['value'];
-				$value = str_replace('</p><p>', "\n\n", $value);
-				$value = strip_tags($value);
-				$this->set_attribute('value', $value);
-			}
-		}
-		$this->set_attribute('type', $type);
-		
-		/**
-		 * TODO
-		 * check status is active
-		 * check visibility equals true (config['visible']
-		 * add delta field (delta is the sort order)
-		 */
-	
-	}
-	
-	protected function render_locked(){
-		$element = "";
-		
-		if ($this->get_item_field()->values){
-			$element .= '<div class="form-control-static">';
-			// text elements cannot output humanized_value since that method 
-			// will strip html-tags like <br>
-			$element .= $this->get_item_field()->values[0]['value'];
-			$element .= '</div>';
-		}
-		
-		return $element;
-	}
-	
-	/**
-	 * Renders the input field
-	 * This method HAS to return parent::render() with an optional element
-	 * string, this way parent::render() can decide whether to display the
-	 * element or not. If it happened to have hidden=true
-	 * @return string
-	 */
-	public function render($element = null, $default_field_decorator = 'field'){
-		// output is:
-		// decorator
-		// element
-		
-		if (!$element){
-			$attributes = $this->get_attributes();
-			// some attributes should not go into the element, like type,
-			// description
-			// required is a special case as well
-			// handle them first
+    public function __construct($app_field, $form, $item_field = null) {
+        parent::__construct($app_field, $form, $item_field);
 
-			$type = $this->get_attribute('type');
-			unset($attributes['type']);
-			unset($attributes['description']);
+        // set type
+        if ($this->app_field->config['settings']['size'] == 'small'){
+            $type = $this->get_attribute('type');
 
-			if ($type == 'text'){
-				$element = '<input type="text"';
-			} else {
-				unset($attributes['value']);
-				$element = '<textarea';
-			}
+        } else {
+            $type = 'textarea';
+            $this->set_attribute('rows', 6);
+            if ($item_field){
+                $value = $item_field->values[0]['value'];
+                $value = str_replace('</p><p>', "\n\n", $value);
+                $value = strip_tags($value);
+                $this->set_attribute('value', $value);
+            }
+        }
+        $this->set_attribute('type', $type);
 
-			$element .= $this->attributes_concat($attributes);
+        /**
+         * TODO
+         * check status is active
+         * check visibility equals true (config['visible']
+         * add delta field (delta is the sort order)
+         */
 
-			$element .= '>';
+    }
 
-			if ($type == 'textarea'){
-				$element .= $this->get_attribute('value');
-				$element .= '</textarea>';
-			}
-		}
-		
-		return parent::render($element);
-	}
+    protected function render_locked(){
+        $element = "";
+
+        if ($this->get_item_field()->values){
+            $element .= '<div class="form-control-static">';
+            // text elements cannot output humanized_value since that method 
+            // will strip html-tags like <br>
+            $element .= $this->get_item_field()->values[0]['value'];
+            $element .= '</div>';
+        }
+
+        return $element;
+    }
+
+    /**
+     * Renders the input field
+     * This method HAS to return parent::render() with an optional element
+     * string, this way parent::render() can decide whether to display the
+     * element or not. If it happened to have hidden=true
+     * @return string
+     */
+    public function render($element = null, $default_field_decorator = 'field'){
+        // output is:
+        // decorator
+        // element
+
+        if (!$element){
+            $attributes = $this->get_attributes();
+            // some attributes should not go into the element, like type,
+            // description
+            // required is a special case as well
+            // handle them first
+
+            $type = $this->get_attribute('type');
+            unset($attributes['type']);
+            unset($attributes['description']);
+
+            $element = '<input type="' . $type . '"';
+
+            if ($type == 'textarea'){
+                unset($attributes['value']);
+                $element = '<textarea';
+            }
+
+            $element .= $this->attributes_concat($attributes);
+
+            $element .= '>';
+
+            if ($type == 'textarea'){
+                $element .= $this->get_attribute('value');
+                $element .= '</textarea>';
+            }
+        }
+
+        return parent::render($element);
+    }
 }
